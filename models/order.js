4a686e866d168d64803d38852d97735f91543ec1
balajiimandi2904 <mc23bt026@gmail.com>
2025-02-01 13:29:38 +0530

    updated

--- models/order.js
+++ models/order.js
@@ -1,10 +1,42 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const orderSchema = new Schema({
+//     user: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'User',
+//         required: true
+//     },
+//     items: [{
+//         item: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'Item'
+//         },
+//         quantity: {
+//             type: Number,
+//             required: true
+//         }
+//     }],
+//     totalAmount: {
+//         type: Number,
+//         required: true
+//     },
+//     createdAt: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
+
+// module.exports = mongoose.model('Order', orderSchema);
+
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId,
-        ref: 'User ',
+        ref: 'User',
         required: true
     },
     items: [{
@@ -24,6 +56,15 @@ const orderSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Received', 'Completed', 'Delivered'],
+        default: 'Pending'
+    },
+    confirmationMessage: {
+        type: String,
+        default: ''
     }
 });
 
--- models/user.js
+++ models/user.js
@@ -1,24 +1,3 @@
-// const mongoose = require('mongoose')
-// const passportLocalMongoose = require('passport-local-mongoose')
-// const Schema = mongoose.Schema
-// const { ref } = require('joi')
-
-// const UserSchema = new Schema({
-//     email : {
-//         type : String ,
-//         required : true ,
-//         unique : true 
-//     },
-//     cart: {
-//         type: Schema.Types.ObjectId,
-//         ref: 'Cart'
-//     }
-// })
-
-// UserSchema.plugin(passportLocalMongoose)
-
-// module.exports = mongoose.model('User',UserSchema)
-
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
